f095c2ca146db3ca77bca9ebaf76a366a93001b5
cthtuf <gdazeysinc@gmail.com>
2021-07-18 12:34:08 +0500

    Remove entrypoint

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     python_requires=">=3.5",
     packages=['telegram_send'],
     install_requires=["python-telegram-bot>=12.1.1", "colorama", "appdirs"],
-    entry_points={"console_scripts": ["telegram-send=telegram_send:main"]},
 
     author="Rahiel Kasim",
     author_email="rahielkasim@gmail.com",
