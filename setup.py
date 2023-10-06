cde7ca03967121dbc0e709ee68d6267bb1b67957
Rahiel Kasim <rahiel.kasim@kabisa.nl>
2023-10-06 23:14:03 +0200

    pin version of python-telegram-bot, release 0.35

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     python_requires=">=3.7",    # also update classifier below
     packages=["telegram_send"],
-    install_requires=["python-telegram-bot>=20.0", "colorama", "appdirs"],
+    install_requires=["python-telegram-bot==20.6", "colorama", "appdirs"],
     entry_points={"console_scripts": ["telegram-send=telegram_send.telegram_send:main"]},
 
     author="Rahiel Kasim",
