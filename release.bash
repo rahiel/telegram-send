fac4350210f5ed5592c372683fc99a41ea009f62
Rahiel Kasim <rahielkasim@gmail.com>
2026-01-14 20:41:43 +0100

    fix generating docs

--- release.bash
+++ release.bash
@@ -5,11 +5,3 @@ set -euo pipefail
 rm -rf dist/
 uv sync
 uv build
-
-# Make documentation
-git checkout gh-pages
-rm -rf docs/
-pdoc -o docs/ telegram_send/
-
-git add docs/
-git commit -m "update docs"
