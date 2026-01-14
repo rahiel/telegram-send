f3d4495f8f7251814bba27926976bb8b269829ad
rahiel <rahiel@users.noreply.github.com>
2026-01-14 19:42:04 +0000

    deploy: fac4350210f5ed5592c372683fc99a41ea009f62

--- telegram_send/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-.. include:: ../README.md
-"""
-from .version import __version__
-from .telegram_send import configure, delete, send
-
-
-__all__ = ["configure", "delete", "send", "__version__"]
